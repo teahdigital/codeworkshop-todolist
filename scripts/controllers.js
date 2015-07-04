0e93cbedc49e5f911bf0153040834d9dc658cee7
Diego Solano <diego_soma@hotmail.com>
2015-07-04 16:51:48 -0600

    api full resp

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -7,7 +7,7 @@
 		
 		$scope.lists = [];
 		API.getLists().success(function (response) {
-            $scope.lists = response;
+            $scope.lists = response.boards;
         }).error(function (error) {
             $scope.status = 'Unable to load lists data: ' + error.message;
         });
@@ -24,9 +24,48 @@
             $scope.status = 'Unable to load tasks data: ' + error.message;
         });
 
+        $scope.users = [];
+		API.getUser().success(function (response) {
+            $scope.users = response;
+        }).error(function (error) {
+            $scope.status = 'Unable to load users data: ' + error.message;
+        });
+
+        /////////////////////////////////////////////////////////////////////
+	    $scope.checked = [];
+
+	    // Add a Item to the list
+	    $scope.addItem = function () {
+	        $scope.users.push({
+	            amount: $scope.itemUser,
+	            name: $scope.itemName
+	        });
+	        // Clear input fields after push
+	        $scope.itemUser = "";
+	        $scope.itemName = "";
+	    };
+
+	    // Add Item to Checked List and delete from Unchecked List
+	    $scope.toggleChecked = function (item, index) {
+	        $scope.checked.push(item);
+	        $scope.users.splice(index, 1);
+	    };
+
+	    // Get Total Items
+	    $scope.getTotalItems = function () {
+	        return $scope.users.length;
+	    };
+
+	    // Get Total Checked Items
+	    $scope.getTotalCheckedItems = function () {
+	        return $scope.checked.length;
+	    };
+
+
+
 	})
 
-	.controller('editCtrl', function($scope, $routeParams){
+	.controller('addList', function($scope, $routeParams){
 		
 	})
 
@@ -35,15 +74,19 @@
 	})
 
 	.factory('API', function($http) {
-		var urlAPI = 'http://localhost/codeworkshop-todolist/data/';
+		var urlAPI = 'http://codeworkshops.teah.digital/todolist/laravel-api/public/';
     	var dataFactory = {};
 
     	dataFactory.getLists = function () {
-	        return $http.get(urlAPI + 'lists.json');
+	        return $http.get(urlAPI + 'boards');
 	    };
 
 	    dataFactory.getTask = function () {
 	        return $http.get(urlAPI + 'tasks.json');
+	    };
+
+	    dataFactory.getUser = function () {
+	        return $http.get(urlAPI + 'users.json');
 	    };
 
 	    return dataFactory;
