9c206e096af6edb7d7a9c1c7efe3ca02121b863e
Diego Solano <diego_soma@hotmail.com>
2015-07-04 10:45:16 -0600

    angular frame

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+(function(){
+	'use strict';
+
+	var app = angular.module('dashboard', ['ngRoute', 'ngAnimate', 'appControllers']);
+
+	app.config(function($routeProvider) {
+		$routeProvider
+
+		.when('/', {
+			templateUrl : 'views/home.html',
+			controller  : 'homeController'
+		})
+
+		.when('/client/:slug/', {
+			templateUrl : 'views/client.html',
+			controller  : 'clientController'
+		})
+
+		.otherwise({
+			redirectTo:'/'
+		});
+	});
+
+	angular.module('appControllers', []);
+	
+})();
