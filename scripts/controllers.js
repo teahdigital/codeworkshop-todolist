2587797cd5f525c9154acd7d82f3d9aa566589db
Diego Solano <diego_soma@hotmail.com>
2015-07-04 13:47:16 -0600

    views controls angular

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -2,43 +2,13 @@
   'use strict';
 	
 	angular.module('appControllers')
-
- // 	.controller('navController', function($scope, $http){
-		
-	// 	// $http.get("http://localhost/projects/github-projects/laravelApps/teah-dashboard/public/clients").success(function(response) {
-	// 	// 	$scope.clients = response.clients;
-	// 	// });
- // 		$http.get("apirest/public/clients").success(function(response) {
-	// 		$scope.clients = response.clients;
-	// 	});
-
-		
-
-	// })
-
-	.controller('homeController', function($scope, $http){
-		console.log("home controller ready");
-
-		// $http.get("apirest/public/projects").success(function(response) {
-		// 	$scope.projects = response.projects;
-		// }); 
-
-	
-
-
-
+ 
+	.controller('homeCtrl', function($scope, $routeParams, $http){
+		console.log('homeCtrl: ready');
 	})
-	
-	// .controller('clientController', ['$scope','$routeParams','$http', function($scope, $routeParams, $http) {
- //  		var slug = $routeParams.slug;
 
-	// 	$http.get("apirest/public/projects/"+slug+"").success(function(response) {
-	// 		$scope.projects = response.projects;
-	// 		$scope.client = response.projects[0].client;
-	// 	}); 
-
-		 
-
-	// }]);
-
-}());
+	.controller('listCtrl', function($scope, $routeParams, $http){
+		$scope.name = $routeParams.name;
+	});
+	 
+})();
